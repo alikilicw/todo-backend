3987ddb796530fd111e14300095d65073c1fd2e6
alikilicw <alikilic.w1@gmail.com>
2025-03-22 02:26:04 +0300

    adjusted timestamps:true and versionKey:false for todo model

--- src/todo/todo.model.ts
+++ src/todo/todo.model.ts
@@ -1,7 +1,7 @@
 import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose'
 import { Document } from 'mongoose'
 
-@Schema()
+@Schema({ timestamps: true, versionKey: false })
 export class Todo extends Document {
     @Prop({ required: true })
     title: string
