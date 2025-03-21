4e28d99493acb594ba9729697188a5562f6f2f02
alikilicw <alikilic.w1@gmail.com>
2025-03-22 02:37:48 +0300

    added todo validation map

---
+++ src/todo/todo.validation.ts
@@ -0,0 +1,18 @@
+import * as Joi from 'joi'
+import BaseValidation from 'src/common/validation/base.validation'
+
+export default class TodoValidation extends BaseValidation {
+    public static create = Joi.object({
+        title: Joi.string().min(5).max(50).required(),
+        description: Joi.string().min(0).max(200).optional()
+    })
+
+    public static find = Joi.object({
+        title: Joi.string().min(5).max(50).optional()
+    })
+
+    public static update = Joi.object({
+        title: Joi.string().min(5).max(50).optional(),
+        description: Joi.string().min(0).max(200).optional()
+    })
+}
