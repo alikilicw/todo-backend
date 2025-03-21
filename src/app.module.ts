6988e39a9e5d96b44d0e224cc41a72f88911ef70
alikilicw <alikilic.w1@gmail.com>
2025-03-22 02:01:55 +0300

    added config module and integrated mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,23 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { TodoModule } from './todo/todo.module';
+import { Module } from '@nestjs/common'
+import { AppController } from './app.controller'
+import { AppService } from './app.service'
+import { TodoModule } from './todo/todo.module'
+import { MongooseModule } from '@nestjs/mongoose'
+import { ConfigModule, ConfigService } from '@nestjs/config'
 
 @Module({
-  imports: [TodoModule],
-  controllers: [AppController],
-  providers: [AppService],
+    imports: [
+        TodoModule,
+        ConfigModule.forRoot({ isGlobal: true }),
+        MongooseModule.forRootAsync({
+            imports: [ConfigModule],
+            inject: [ConfigService],
+            useFactory: (configService: ConfigService) => ({
+                uri: configService.get<string>('MONGO_URL')
+            })
+        })
+    ],
+    controllers: [AppController],
+    providers: [AppService]
 })
 export class AppModule {}
