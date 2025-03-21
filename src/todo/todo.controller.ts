91ba92dde570088dd3e50a5f15cc4eea38560d54
alikilicw <alikilic.w1@gmail.com>
2025-03-22 02:19:38 +0300

    filled todo controller

--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -1,4 +1,33 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common'
+import { TodoService } from './todo.service'
+import { CreateTodoDto, updateTodoDto } from './todo.dto'
 
 @Controller('todo')
-export class TodoController {}
+export class TodoController {
+    constructor(private readonly todoService: TodoService) {}
+
+    @Post()
+    async create(@Body() createTodoDto: CreateTodoDto) {
+        return this.todoService.create(createTodoDto)
+    }
+
+    @Get()
+    async findAll() {
+        return this.todoService.findAll()
+    }
+
+    @Get(':id')
+    async findById(@Param('id') id: string) {
+        return this.todoService.findById(id)
+    }
+
+    @Patch(':id')
+    async update(@Param('id') id: string, @Body() updateTodoDto: updateTodoDto) {
+        return this.todoService.update(id, updateTodoDto)
+    }
+
+    @Delete(':id')
+    async delete(@Param('id') id: string) {
+        return this.todoService.delete(id)
+    }
+}
