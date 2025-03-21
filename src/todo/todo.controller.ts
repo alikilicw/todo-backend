24c0a823e65d02000614b4e79a550d5fe8fd0f5c
alikilicw <alikilic.w1@gmail.com>
2025-03-22 02:46:57 +0300

    added validations to controllers

--- src/todo/todo.controller.ts
+++ src/todo/todo.controller.ts
@@ -1,12 +1,15 @@
-import { Body, Controller, Delete, Get, Param, Patch, Post } from '@nestjs/common'
+import { Body, Controller, Delete, Get, Param, Patch, Post, UsePipes } from '@nestjs/common'
 import { TodoService } from './todo.service'
 import { CreateTodoDto, updateTodoDto } from './todo.dto'
+import { JoiValidationPipe } from 'src/common/validation/validation.pipe'
+import TodoValidation from './todo.validation'
 
 @Controller('todos')
 export class TodoController {
     constructor(private readonly todoService: TodoService) {}
 
     @Post()
+    @UsePipes(new JoiValidationPipe({ bodySchema: TodoValidation.create }))
     async create(@Body() createTodoDto: CreateTodoDto) {
         return this.todoService.create(createTodoDto)
     }
@@ -17,17 +20,22 @@ export class TodoController {
     }
 
     @Get(':id')
-    async findById(@Param('id') id: string) {
-        return this.todoService.findById(id)
+    @UsePipes(new JoiValidationPipe({ paramSchema: TodoValidation.id }))
+    async findById(@Param('id') params: { id: string }) {
+        return this.todoService.findById(params.id)
     }
 
     @Patch(':id')
-    async update(@Param('id') id: string, @Body() updateTodoDto: updateTodoDto) {
-        return this.todoService.update(id, updateTodoDto)
+    @UsePipes(
+        new JoiValidationPipe({ paramSchema: TodoValidation.id, bodySchema: TodoValidation.update })
+    )
+    async update(@Param('id') params: { id: string }, @Body() updateTodoDto: updateTodoDto) {
+        return this.todoService.update(params.id, updateTodoDto)
     }
 
     @Delete(':id')
-    async delete(@Param('id') id: string) {
-        return this.todoService.delete(id)
+    @UsePipes(new JoiValidationPipe({ paramSchema: TodoValidation.id }))
+    async delete(@Param('id') params: { id: string }) {
+        return this.todoService.delete(params.id)
     }
 }
